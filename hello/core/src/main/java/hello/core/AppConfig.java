b65b939b7a2c967da119a333820754f46884a2ec
JWbase <jungwoo1002@gmail.com>
2023-03-13 00:59:46 +0900

    - 싱글톤 스프링컨테이너 테스트

--- hello/core/src/main/java/hello/core/AppConfig.java
+++ hello/core/src/main/java/hello/core/AppConfig.java
@@ -19,16 +19,21 @@ public class AppConfig {
     // 빈 이름은 항상 다른 이름을 부여해야 한다. 다른빈이 무시 되거나 기존 빈을 덮어 버릴 수도 있다.
     @Bean(name = "memberService")
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        //1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        //2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
--- hello/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ hello/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,6 +10,12 @@ public class OrderServiceImpl implements OrderService {
     // 아래 코드는 기능을 확장해서 변경하면, 클라이언트 코드에 영향을 주기 때문에 OCP를 위반
     // 인터페이스가 아닌 구현 클래스를 의존하고 있기 때문에 DIP도 위반
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
     private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
