8ec6358804e0aa0fdb4fbcf1a29c15b3eff546f9
jwBase <jungwoo1002@gmail.com>
2023-03-13 17:22:18 +0900

    - 컴포넌트 스캔

--- hello/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ hello/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,9 +1,12 @@
 package hello.core.member;
 
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     // HashMap은 동시성 이슈가 발생할 수 있어 ConcurrentHashMap을 사용하는 것을 추천
