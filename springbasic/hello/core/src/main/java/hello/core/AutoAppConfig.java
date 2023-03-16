a90864426d75a01d5df298973e08f7d117af2fd8
jwBase <jungwoo1002@gmail.com>
2023-03-16 09:50:01 +0900

    - @Qualifier, @Primary 사용

--- springbasic/hello/core/src/main/java/hello/core/AutoAppConfig.java
+++ springbasic/hello/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
