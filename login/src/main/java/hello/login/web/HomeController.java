29eb8d01ec66c7ac4ea804c5e02f30f64b4999db
jwBase <jungwoo1002@gmail.com>
2023-04-13 10:56:35 +0900

    - 인터셉터 구현

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -91,12 +91,12 @@ public class HomeController {
     @GetMapping("/")
     public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
 
-        //세션에 회원 데이터가 없으면 home
+        // 세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
             return "home";
         }
 
-        //세션이 유지 되면 로그인으로 이동
+        // 세션이 유지 되면 로그인으로 이동
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
