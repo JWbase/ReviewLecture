e4f236940c544b5354dad0a80a126d29a95a683e
JWbase <jungwoo1002@gmail.com>
2023-07-16 19:29:21 +0900

    연관관계 설정

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")//mappedBy 나는 연관관계 주인이 아니라 거울입니다.
+    private List<Order> orders = new ArrayList<>();
+}
